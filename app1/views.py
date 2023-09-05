fb659f9be31c2dc0e4b60a35f15f8a2db9d2d731
valentina buoro <valentinabuoro4@gmail.com>
2023-09-05 16:38:23 +0100

    simple word counter

--- app1/views.py
+++ app1/views.py
@@ -7,10 +7,16 @@ def index(request):
     #from a database, it will be something like
     # name = user.name
     # if we have multiple data, we can use context
-    context = {
-        'name': 'Patrick',
-        'age': 25,
-        'nationality':'Nigerian'
-    }
+    #context = {
+    #    'name': 'Patrick',
+    #    'age': 25,
+    #    'nationality':'Nigerian'
+    #}
 
-    return render(request,'index.html', context)
+    return render(request,'index.html') #add context inside the parentheses to render the data
+
+
+def counter(request):
+    text = request.GET['text']
+    amount_of_words= len(text.split())
+    return render(request,'counter.html' , {'amount': amount_of_words})
