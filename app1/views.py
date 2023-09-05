c77ae49de596fbeab8a66bb6710c89f9239ffd0e
valentina buoro <valentinabuoro4@gmail.com>
2023-09-05 16:08:31 +0100

    sending dynamic data to template file

--- app1/views.py
+++ app1/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    #name= 'Patrick'
+    #from a database, it will be something like
+    # name = user.name
+    # if we have multiple data, we can use context
+    context = {
+        'name': 'Patrick',
+        'age': 25,
+        'nationality':'Nigerian'
+    }
+
+    return render(request,'index.html', context)
