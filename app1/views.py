0ff68b5084b3cb423a81c19ce7e0405f27f05593
valentina buoro <valentinabuoro4@gmail.com>
2023-09-10 16:25:25 +0100

    models in python

--- app1/views.py
+++ app1/views.py
@@ -19,7 +19,29 @@ def index(request):
     feature1.name = 'fast'
     feature1.details = ' Our service is fast'
 
-    return render(request,'index.html') #add context inside the parentheses to render the data
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'reliable'
+    feature2.details = ' Our service is reliable'
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'easy to use'
+    feature3.details = ' Our service is easy to use'
+
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'affordable'
+    feature4.details = ' Our service is affordable'
+
+    feature5 = Feature()
+    feature5.id = 4
+    feature5.name = 'trustworthy'
+    feature5.details = ' Our service is trustworthy'
+
+    features = [feature1, feature2, feature3, feature4, feature5]
+
+    return render(request,'index.html', {'features' : features,}) #add context inside the parentheses to render the data
 
 
 def counter(request):
