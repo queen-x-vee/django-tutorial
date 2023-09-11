2739ac951ddec2a1fee441571ff3f9005914b8d2
valentina buoro <valentinabuoro4@gmail.com>
2023-09-11 17:22:58 +0100

    user login and logout

--- app1/models.py
+++ app1/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Feature:
-    id: int
-    name: str
-    details: str
+class Feature(models.Model):
+    #id: int
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=500)
+   
--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth 
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
@@ -13,38 +15,89 @@ def index(request):
     #    'age': 25,
     #    'nationality':'Nigerian'
     #}
-    
-    feature1 = Feature()
+    '''
     feature1.id = 0
     feature1.name = 'fast'
+    feature1 = Feature()
     feature1.details = ' Our service is fast'
+    feature1.is_true = True
 
     feature2 = Feature()
     feature2.id = 1
     feature2.name = 'reliable'
     feature2.details = ' Our service is reliable'
+    feature2.is_true = True
 
     feature3 = Feature()
     feature3.id = 2
     feature3.name = 'easy to use'
     feature3.details = ' Our service is easy to use'
+    feature3.is_true = False
 
     feature4 = Feature()
     feature4.id = 3
     feature4.name = 'affordable'
     feature4.details = ' Our service is affordable'
+    feature4.is_true = True
 
-    feature5 = Feature()
-    feature5.id = 4
-    feature5.name = 'trustworthy'
-    feature5.details = ' Our service is trustworthy'
-
-    features = [feature1, feature2, feature3, feature4, feature5]
-
+    features = [feature1, feature2, feature3, feature4]
+    '''
+    features = Feature.objects.all()
     return render(request,'index.html', {'features' : features,}) #add context inside the parentheses to render the data
 
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email = email).exists():
+                messages.info(request, 'Email already in use')
+                return redirect('register')
+            
+            elif User.objects.filter(username = username).exists():
+                messages.info(request, 'Username already in use')
+ 
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+            
+        else:
+            messages.info(request, 'Password not the same')
+            return redirect('register')
+        
+    else:
+        return render(request, 'register.html')
+
+
+    return render(request, 'register.html')
+
+def login (request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username = username, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        
+        else:
+            messages.info(request, 'Credentials Invalid')
+
+    else:
+        return render (request, 'login.html')
+
+def logout (request):
+    auth.logout(request)
+    return redirect('/')
 
 def counter(request):
     text = request.POST['text']
     amount_of_words= len(text.split())
     return render(request,'counter.html' , {'amount': amount_of_words})
+
